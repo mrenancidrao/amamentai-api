4144d0eb54c29a5f780e5928008baed77f875b87
Renan Cidrão <mrenancidrao@gmail.com>
2018-03-19 23:32:59 -0300

    first commit

---
+++ src/main/java/com/example/amamentai/api/AmamentaiApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.amamentai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmamentaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmamentaiApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/amamentai/api/AmamentaiApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.amamentai.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmamentaiApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
