8f60a620b45311fbb236214c37fe3e7063ef72cc
renan <mrenancidrao@gmail.com>
2018-03-26 15:43:05 -0300

    Correcao arquivo migracao

--- src/main/resources/db/migration/V01__criar_e_registrar_schema.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_schema.sql
@@ -1,39 +1,4 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 9.6.6
--- Dumped by pg_dump version 9.6.6
-
--- Started on 2018-03-26 01:06:50
-
---
--- TOC entry 1 (class 3079 OID 12387)
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- TOC entry 2399 (class 0 OID 0)
--- Dependencies: 1
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- TOC entry 185 (class 1259 OID 32789)
--- Name: agenda; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE agenda (
     id integer NOT NULL,
@@ -609,7 +574,7 @@ ALTER TABLE pessoa OWNER TO postgres;
 --
 -- TOC entry 208 (class 1259 OID 32878)
 -- Name: pessoa_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
+--p
 
 CREATE SEQUENCE pessoa_id_seq
     START WITH 1
