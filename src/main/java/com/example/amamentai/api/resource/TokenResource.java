ebfaf404c9ab5aa5ee553de8d94629581917d7eb
renan <mrenancidrao@gmail.com>
2018-05-16 16:38:40 -0300

    Correção de caminho do resource token (de /token para /tokens)

--- src/main/java/com/example/amamentai/api/resource/TokenResource.java
+++ src/main/java/com/example/amamentai/api/resource/TokenResource.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.amamentai.api.config.property.AmamentaiApiProperty;
 
 @RestController
-@RequestMapping("/token")
+@RequestMapping("/tokens")
 public class TokenResource {
 	
 	@Autowired
