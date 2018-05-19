7ac928a95985f20c4b993d3459692495db6dce93
Renan Cidrão <mrenancidrao@gmail.com>
2018-05-18 22:45:52 -0300

    Cors

--- src/main/java/com/example/amamentai/api/cors/CorsFilter.java
+++ src/main/java/com/example/amamentai/api/cors/CorsFilter.java
@@ -25,7 +25,7 @@ public class CorsFilter implements Filter {
 	@Autowired
 	private AmamentaiApiProperty amamentaiApiProperty;
 
-	@Override
+	/*@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
 		
@@ -58,6 +58,36 @@ public class CorsFilter implements Filter {
 	public void init(FilterConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
 		
-	}
+	}*/
+	
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+
+        response.setHeader("Access-Control-Allow-Origin", amamentaiApiProperty.getOriginPermitida());
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equals(request.getMethod()) && amamentaiApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
 
 }
