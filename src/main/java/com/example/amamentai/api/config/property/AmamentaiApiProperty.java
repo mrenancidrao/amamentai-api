540553a47b2d8e90cb0e525f27f2520d4a855144
renan <mrenancidrao@gmail.com>
2018-05-21 11:49:49 -0300

    Adicionar origin https no cors

--- src/main/java/com/example/amamentai/api/config/property/AmamentaiApiProperty.java
+++ src/main/java/com/example/amamentai/api/config/property/AmamentaiApiProperty.java
@@ -8,7 +8,7 @@ public class AmamentaiApiProperty {
 
 	//private String originPermitida = "http://localhost:4200";
 	
-	private String originPermitida = "*";
+	private String originPermitida = "https://amamentai.herokuapp.com";
 	
 	
 	private final Seguranca seguranca = new Seguranca();
